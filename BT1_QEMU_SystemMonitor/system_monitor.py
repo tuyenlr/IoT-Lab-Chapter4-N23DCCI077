9de576b8bc5e17a68ef548f5ae3447329aeda246
NguyenTuyen <tuyenntb.io@gmail.com>
2026-04-22 19:45:55 +0700

    Fix syntax error in BT1

--- BT1_QEMU_SystemMonitor/system_monitor.py
+++ BT1_QEMU_SystemMonitor/system_monitor.py
@@ -2,46 +2,47 @@ import psutil
 from datetime import datetime
 from time import sleep
 
-log_file = open('system_log.txt','w')
+# Mo file de ghi
+log_file = open('system_log.txt', 'w')
+
 try:
-   while True:
-      # Doc CPU
-      cpu_list = psutil.cpu_percent(interval=1,percpu=True)
-      cpu_avg = sum(cpu_list)/len(cpu_list)
-      if cpu_avg>=70:
-         status = 'CRITICAL'
-      elif cpu_avg>=30:
-         status = 'WARNING'
-      else:
-         status = 'NORMAL'
-      # Doc RAM
-      ram = psutil.virtual_memory()
-      ram_used_mb = ram.used //(1024**2)
-      ram_total_mb = ram.total //(1024**2)
-      ram_pct = ram.percent
+    while True:
+        # 1. Doc thong so CPU
+        cpu_list = psutil.cpu_percent(interval=1, percpu=True)
+        cpu_avg = sum(cpu_list) / len(cpu_list)
+        
+        if cpu_avg >= 70:
+            status = 'CRITICAL'
+        elif cpu_avg >= 30:
+            status = 'WARNING'
+        else:
+            status = 'NORMAL'
 
-      # Doc disk
-      disk = psutil.disk_usage('/')
-      disk_pct = disk.percent
+        # 2. Doc thong so RAM
+        ram = psutil.virtual_memory()
+        ram_used_mb = ram.used // (1024**2)
+        ram_total_mb = ram.total // (1024**2)
 
-      #Dinh dang thoi gian
-      now = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+        # 3. Dinh dang thoi gian
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+        # 4. Tao dong output (Nho dau nhay don o cuoi)
+        line = f'[{now}] CPU: {cpu_avg:.1f}% | RAM: {ram_used_mb}/{ram_total_mb} MB'
 
-      # Tao dong output chuan
-      line = f'[{now}] CPU: {cpu_avg:.1f}% | RAM: {ram_used_mb}/{ram_total_mb} M'
+        # 5. In ra man hinh
+        print(line)
+        if status != 'NORMAL':
+            print(f' {status}: CPU dang o {cpu_avg:.1f}%')
 
-      print(line)
-      if status != 'NORMAL':
-         print(f' {status}: CPU dang o {cpu_avg:.1f}%')
-         log_file.write(line+'\n')
-         log_file.flush()
+        # 6. Ghi vao file (Dòng này phai thang hang voi print o tren)
+        log_file.write(line + '\n')
+        log_file.flush()
 
-         # Lap lai sau moi 2s
-         sleep(2)
+        # 7. Nghi 2 giay roi moi lap lai (Rat quan trong!)
+        sleep(2)
 
 except KeyboardInterrupt:
-   print('\nDung giam sat.')
+    print('\nDung giam sat.')
 finally:
-   log_file.close()
-   print('Log saved to system_log.txt')
+    log_file.close()
+    print('Log saved to system_log.txt')
