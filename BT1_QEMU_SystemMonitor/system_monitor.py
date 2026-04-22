9a1f07192ba7e51043e33ba83c3fbee5b0699c39
NguyenTuyen <tuyenntb.io@gmail.com>
2026-04-22 19:53:58 +0700

    Fix linting errors W292 and W293

--- BT1_QEMU_SystemMonitor/system_monitor.py
+++ BT1_QEMU_SystemMonitor/system_monitor.py
@@ -10,7 +10,7 @@ try:
         # 1. Doc thong so CPU
         cpu_list = psutil.cpu_percent(interval=1, percpu=True)
         cpu_avg = sum(cpu_list) / len(cpu_list)
-        
+
         if cpu_avg >= 70:
             status = 'CRITICAL'
         elif cpu_avg >= 30:
@@ -26,7 +26,7 @@ try:
         # 3. Dinh dang thoi gian
         now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        # 4. Tao dong output (Nho dau nhay don o cuoi)
+        # 4. Tao dong output
         line = f'[{now}] CPU: {cpu_avg:.1f}% | RAM: {ram_used_mb}/{ram_total_mb} MB'
 
         # 5. In ra man hinh
@@ -34,11 +34,11 @@ try:
         if status != 'NORMAL':
             print(f' {status}: CPU dang o {cpu_avg:.1f}%')
 
-        # 6. Ghi vao file (Dòng này phai thang hang voi print o tren)
+        # 6. Ghi vao file
         log_file.write(line + '\n')
         log_file.flush()
 
-        # 7. Nghi 2 giay roi moi lap lai (Rat quan trong!)
+        # 7. Nghi 2 giay roi moi lap lai
         sleep(2)
 
 except KeyboardInterrupt:
